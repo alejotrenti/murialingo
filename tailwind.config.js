099421cb37120e5c88d973011836ee7c44b082ee
alejo <163149710+alejotrenti@users.noreply.github.com>
2024-08-06 19:26:58 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // o 'media' si prefieres usar la preferencia del sistema
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
